e9eae7acd396ad3213949fe531be52790974cfa3
yassyolo <yoana.yotova03@gmail.com>
2024-10-05 15:33:22 +0300

    Added data entities and configured relationships between them

---
+++ src/main/java/com/example/java78/entities/RecentSearch.java
@@ -0,0 +1,24 @@
+package com.example.java78.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recent_searches")
+public class RecentSearch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "search_term", nullable = false)
+    private String searchTerm;
+
+    @Column(name = "search_date", nullable = false)
+    private LocalDateTime searchDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+}
