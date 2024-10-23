7446d278468aa3adb686cad4288ab4fe34dc5609
Denis <piwkillpie@gmail.com>
2024-10-23 09:45:19 +0300

    Added admin user creation in main class

--- src/main/java/com/example/SpringShop/SpringShopApplication.java
+++ src/main/java/com/example/SpringShop/SpringShopApplication.java
@@ -1,13 +1,45 @@
 package com.example.SpringShop;
 
+import com.example.SpringShop.Constants.UserRoleConstants;
+import com.example.SpringShop.Entities.User;
+import com.example.SpringShop.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringShopApplication {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SpringShopApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringShopApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner createAdminUser() {
+        return args -> {
+            if (userRepository.findByUsername("admin") == null) {
+                User admin = new User();
+                admin.setUsername("admin");
+                admin.setPassword(passwordEncoder().encode("admin123"));
+                admin.setRole(UserRoleConstants.ADMIN);
+                userRepository.save(admin);
+            }
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
